87f48162dcc56dad4dd71c294765457120c0f47a
Brad <bradwrage@gmail.com>
2014-07-31 11:42:14 -0700

    auto commit (mixture)

---
+++ assets/js/vendor/submit.js
@@ -0,0 +1,41 @@
+transitionToThankYou = function() {
+  jQuery(".js-rsvp-interaction").hide()
+  jQuery(".js-rsvp-interaction--complete").show()
+  $("#hidden-iframe").off('load', transitionToThankYou)
+}
+
+matchHeightsForRsvpStates = function() {
+  height = jQuery(".js-rsvp-interaction--form").height();
+  jQuery(".js-rsvp-interaction").height(height);
+}
+
+selectRsvp = function() {
+  $this = jQuery(this)
+  jQuery(".js-rsvp-target").removeClass("js-rsvp--selected")
+  $this.addClass("js-rsvp--selected")
+
+  jQuery("#js-rsvp-input").val($this.data('rsvpValue'));
+}
+
+submitForm = function(event){
+  event.preventDefault();
+
+  $("#hidden-iframe").on('load', transitionToThankYou)
+  transitionToInTransit()
+
+  $('#js-rsvp-the-form').submit();
+}
+
+transitionToInTransit = function() {
+  jQuery(".js-rsvp-submit").hide()
+  jQuery(".js-rsvp-interaction").hide()
+  jQuery(".js-rsvp-interaction--in-transit").show()
+}
+
+jQuery(function(){
+  matchHeightsForRsvpStates()
+
+  jQuery(".js-rsvp-target").click(selectRsvp)
+  jQuery(".js-rsvp-submit").click(submitForm)
+});
+
