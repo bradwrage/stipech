4526af9ffb333357080485e0065029e4c4dc9381
Dan Ott <thedanielott@gmail.com>
2014-07-30 21:02:46 -0700

    All wired up.

--- assets/js/vendor/submit.js
+++ assets/js/vendor/submit.js
@@ -1,21 +1,53 @@
-  window.addEventListener("message", postMessageReceived, false);
+var postMessagesReceived = 0;
 
-      function postMessageReceived()
-      {
-        console.log('postMessageReceived', arguments);
-      }
+var listenForHiddenIframeLoad = function(event) {
+  if (event.data !== "hidden-iframe-loaded") {
+    return false;
+  }
 
-      jQuery(function(){
-        jQuery(".js-rsvp-target").click(function(){
-          $this = jQuery(this)
-          jQuery(".js-rsvp-target").removeClass("js-rsvp--selected")
-          $this.addClass("js-rsvp--selected")
+  postMessagesReceived += 1;
 
-          jQuery("#js-rsvp-input").val($this.data('rsvpValue'));
-        });
+  if (postMessagesReceived === 2) {
+    transitionToThankYou()
+  }
+}
 
-        jQuery(".js-rsvp-submit").click(function(event) {
-          event.preventDefault();
-          $(this).closest('form').submit();
-        });
-      });
+transitionToThankYou = function() {
+  jQuery(".js-rsvp-interaction").hide()
+  jQuery(".js-rsvp-interaction--complete").show()
+}
+
+matchHeightsForRsvpStates = function() {
+  height = jQuery(".js-rsvp-interaction--form").height();
+  jQuery(".js-rsvp-interaction").height(height);
+}
+
+selectRsvp = function() {
+  $this = jQuery(this)
+  jQuery(".js-rsvp-target").removeClass("js-rsvp--selected")
+  $this.addClass("js-rsvp--selected")
+
+  jQuery("#js-rsvp-input").val($this.data('rsvpValue'));
+}
+
+submitForm = function(event){
+  event.preventDefault();
+  $('#js-rsvp-the-form').submit();
+}
+
+transitionToInTransit = function() {
+  jQuery(".js-rsvp-submit").hide()
+  jQuery(".js-rsvp-interaction").hide()
+  jQuery(".js-rsvp-interaction--in-transit").show()
+}
+
+jQuery(function(){
+  window.addEventListener("message", listenForHiddenIframeLoad, false);
+
+  matchHeightsForRsvpStates()
+
+  jQuery(".js-rsvp-target").click(selectRsvp)
+  jQuery(".js-rsvp-submit").click(submitForm)
+  jQuery(".js-rsvp-submit").click(transitionToInTransit)
+});
+
